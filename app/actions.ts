f3ab9649b9be32e452251fabdf55bc9aa63cdb3c
Joe Clark <weirdlookingjay@live.com>
2024-10-03 11:57:01 -0400

    Created Event Types page

--- app/actions.ts
+++ app/actions.ts
@@ -3,7 +3,11 @@
 import prisma from "./lib/db";
 import { requireUser } from "./lib/hooks";
 import { parseWithZod } from "@conform-to/zod";
-import { onboardingSchemaValidation, settingsSchema } from "./lib/zodSchemas";
+import {
+  eventTypeSchema,
+  onboardingSchemaValidation,
+  settingsSchema,
+} from "./lib/zodSchemas";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 
@@ -146,4 +150,32 @@ export async function updateAvailabilityAction(formData: FormData) {
   } catch (error) {
     console.log(error);
   }
+}
+
+export async function CreateEventTypeAction(
+  prevState: any,
+  formData: FormData
+) {
+  const session = await requireUser();
+
+  const submission = parseWithZod(formData, {
+    schema: eventTypeSchema,
+  });
+
+  if (submission.status !== "success") {
+    return submission.reply();
+  }
+
+  await prisma.eventType.create({
+    data: {
+      title: submission.value.title,
+      duration: submission.value.duration,
+      url: submission.value.url,
+      description: submission.value.description,
+      videoCallSoftware: submission.value.videoCallSoftware,
+      userId: session.user?.id,
+    },
+  });
+
+  return redirect("/dashboard");
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,128 @@
+import { notFound } from "next/navigation";
+import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
+import { EmptyState } from "../components/EmptyState";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ExternalLink, Link2, Pen, Settings, Trash, Users2 } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+
+async function getData(userId: string) {
+    const data = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        select: {
+            userName: true,
+            eventType: {
+                select: {
+                    id: true,
+                    active: true,
+                    title: true,
+                    url: true,
+                    duration: true,
+                },
+            },
+        },
+    });
+
+    if (!data) {
+        return notFound();
+    }
+
+    return data;
+}
 
 export default async function DashboardPage() {
     const session = await requireUser();
+    const data = await getData(session.user?.id as string);
 
     return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+        <>
+            {data.eventType.length === 0 ? (
+                <EmptyState
+                    title="You have no Event Types"
+                    description="You can create yuor first event type by clicking the button below."
+                    buttonText="Add evnt type"
+                    href="/dashboard/new"
+                />
+            ) : (
+                <>
+                    <div className="flex items-center justify-between px-2">
+                        <div className="hidden sm:grid gap-y-1">
+                            <h1 className="text3xl md:text-4xl font-semibold">Event Types</h1>
+                            <p className="text-muted-foreground">Create and manage your event types right here.</p>
+                        </div>
+                        <Button asChild>
+                            <Link href="/dashboard/new">
+                                Create New Event
+                            </Link>
+                        </Button>
+                    </div>
+
+                    <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
+                        {data.eventType.map((item) => (
+                            <div className="overflow-hidden shadow rounded-lg border relative" key={item.id}>
+
+                                <div className="absolute top-2 right-2">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Button variant="outline" size="icon">
+                                                <Settings className="size-4" />
+                                            </Button>
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent align="end">
+                                            <DropdownMenuLabel>Event</DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuGroup>
+                                                <DropdownMenuItem asChild>
+                                                    <Link href={`/${data.userName}/${item.url}`}>
+                                                        <ExternalLink className="mr-2 size-4" />
+                                                        Preview
+                                                    </Link>
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    <Link2 className="mr-2 size-4" />
+                                                    Copy
+                                                </DropdownMenuItem>
+                                                <DropdownMenuItem>
+                                                    <Pen className="mr-2 size-4" />
+                                                    Edit
+                                                </DropdownMenuItem>
+                                            </DropdownMenuGroup>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuItem>
+                                                <Trash className="mr-2 size-4" />
+                                                Delete
+                                            </DropdownMenuItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
+
+                                <Link href={`/dashboard/${item.id}`} className="flex items-center p-5">
+                                    <div className="flex-shrink-0">
+                                        <Users2 className="size-6" />
+                                    </div>
+
+                                    <div className="ml-5 w-0 flex-1">
+                                        <dl>
+                                            <dt className="text-sm font-medium text-muted-foreground">{item.duration} Minutes Meeting</dt>
+                                        </dl>
+                                        <dd className="text-lg font-medium">
+                                            {item.title}
+                                        </dd>
+                                    </div>
+                                </Link>
+                                <div className="flex bg-muted px-5 py-3 justify-between items-center">
+                                    <Switch />
+                                    <Button>Edit Event</Button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </>
+            )}
+        </>
+    );
 }
